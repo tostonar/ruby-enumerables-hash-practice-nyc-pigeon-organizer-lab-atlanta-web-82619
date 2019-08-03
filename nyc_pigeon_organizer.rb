3d78f3bfa218564d35d807113a785e7aebb773ff
Andrea Toston <tostonar@gmail.com>
2019-08-03 00:16:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   result = {}
   data.each do |categories, attributes|
-    categories.each do |attribute, names|
+    attributes.each do |attribute, names|
       names.each do |name|
         result[name] = {
           color: [],
