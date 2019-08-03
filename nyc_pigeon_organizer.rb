bdd47300269ee07124d9cc891e1dd552ba889206
Andrea Toston <tostonar@gmail.com>
2019-08-03 00:13:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   result = {}
+  data.each do |categories, attributes|
+    categories.each do |attribute, names|
+      names.each do |name|
+        result[name] = {
+          color: [],
+          gender: [],
+          lives: []
+        }
+      end
+    end
+  end
 end
